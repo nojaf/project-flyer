d9cdd406a072a21730a29b72f11dab66b185c901
Florian Verdonck <Florian Verdonck>
2016-08-29 18:31:24 +0200

    The Flyer and Photos Relationship

---
+++ app/Flyer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Flyer extends Model
+{
+    /**
+     * A flyer is composed of many photos
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function photos()
+    {
+        return $this->hasMany(Photo::class);
+    }
+}
